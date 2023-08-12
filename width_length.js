585e1f7733c2cd581127cca1926dc97104440dbf
0l1xscr1pt <scr1ptkyu01@gmail.com>
2023-08-12 04:22:12 -1000

    reorganized commit

--- width_length.js
+++
@@ -1,9 +0,0 @@
-const areaOrPerimeter = function(length , width) {
-    if (length === width) {
-        let area = length * width;
-        return area;
-    } else {
-        let perimeter = (length + width) * 2;
-        return perimeter;
-    }
-}
