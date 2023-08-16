11c8b8cb283cb2285e583fdcbf645c65bfabedaf
0l1xscr1pt <scr1ptkyu01@gmail.com>
2023-08-15 22:09:34 -1000

    add split odd and even in js

---
+++ finish-js/split_odd_and_even.js
@@ -0,0 +1,24 @@
+function split_odd_and_even(string) {
+    let i = 0;
+    let j = 0;
+    let array = [];
+    let length = (string.length) / 2;
+
+    if (string[j] === "") {
+        return array;
+    }
+    while (i < length) {
+        array[i] = " ";
+        array[i] = string[j];
+        j++;
+
+        if (string[j] === undefined ) {
+            array[i] += "_";
+        } else {
+        array[i] += string[j];
+        }
+        i++;
+        j++;
+    }
+    return array;
+}
